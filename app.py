4a69828e4c9dbe167512a288faa96897d482f034
manav108-hub <manavadwani86@gmail.com>
2025-05-19 16:19:58 +0000

    updated

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origins=["https://ai-species.netlify.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
